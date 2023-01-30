9e7d404ca239ca7d5aaca3fe27c354c27a284b37
kartik nuli <nulikarthik@gmail.com>
2023-01-29 16:54:51 -0800

    make email and telephone quickable

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -11,7 +11,7 @@ class Nav extends React.Component<any, any> {
 
         return (
             <div className="home-page-nav-bar-root">
-                <div className="home-page-nav-bar-brand"><img src={brandImage} /></div>
+                <a href="/" className="home-page-nav-bar-brand"><img src={brandImage} /></a>
                 <div className="home-page-nav-break-mobile-view">
                     <div className="home-page-nav-bar-link-menu">
                         <Link to="/" className="home-page-nav-bar-link">Home</Link>
@@ -19,10 +19,10 @@ class Nav extends React.Component<any, any> {
                     </div>
                     <div className="home-page-nav-bar-right-part">
                         <img className="home-page-nav-bar-email" src={email} />
-                        <div className="home-page-nav-bar-email-address">marketing@vendidollc.com</div>
+                        <a href="mailto:marketing@vendidollc.com" className="home-page-nav-bar-email-address">marketing@vendidollc.com</a>
                         <img className="home-page-nav-bar-ellipse" src={ellipse} />
                         <img className="home-page-nav-bar-telephone" src={telephone} />
-                        <div className="home-page-nav-bar-telephone-number">+1-888-VENDIDO</div>
+                        <a href="tel:18888363436" className="home-page-nav-bar-telephone-number">+1-888-VENDIDO</a>
                     </div>
                 </div>
             </div>
